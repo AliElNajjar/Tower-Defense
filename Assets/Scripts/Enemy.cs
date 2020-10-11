f7457b6e6c2f6e82111b80d642226f64561c7926
maestro-007 <alinajjar.91@hotmail.com>
2020-10-11 14:03:35 +0300

    update

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private Transform target;
+    private int waypointIndex = 0;
+
+    private void Start()
+    {
+        target = Waypoints.waypoints[0];
+    }
+
+    private void Update()
+    {
+        Vector3 direction = target.position - transform.position;
+        transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.3f)
+        {
+            GetNextWaypoint();
+        }
+    }
+
+    void GetNextWaypoint()
+    {
+        if (waypointIndex >= Waypoints.waypoints.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        waypointIndex++;
+        target = Waypoints.waypoints[waypointIndex];
+        
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Spawner : MonoBehaviour
+{
+    public Transform enemy;
+    public Transform spawnPoint;
+    public float restTime = 5f;
+    private float countdown = 2f;
+    public Text timerText;
+    private int waveIndex = 0;
+
+    private void Update()
+    {
+        if (countdown <= 0f)
+        {
+            StartCoroutine(Spawn());
+            countdown = restTime;
+        }
+
+        countdown -= Time.deltaTime;
+
+        timerText.text = Mathf.Round(countdown).ToString();
+    }
+
+    IEnumerator Spawn()
+    {
+        waveIndex++;
+        for (int i = 0; i < waveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemy, spawnPoint.position, spawnPoint.rotation);
+    }
+
+}
